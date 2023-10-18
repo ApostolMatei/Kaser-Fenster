83a71b3e9c0033f0baa2fba31410c7ce4897b6ee
mateias109 <mateias109@gmail.com>
2023-10-18 17:34:17 +0300

    123

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,10 @@ export default defineConfig({
     vue(),
     // ...
     Pages({
-      onRoutesGenerated: (routes) => generateSitemap({ routes })
+      onRoutesGenerated: () => {
+        const explicitRoutes = ['/', '/ueber-uns', '/leistungen', '/produkte', '/kontakt']
+        generateSitemap({ routes: explicitRoutes })
+      }
     })
   ],
   resolve: {
