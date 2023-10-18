b960e80bbf717648432b0490adc4ff05c621e807
mateias109 <mateias109@gmail.com>
2023-10-18 18:08:48 +0300

    123

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-import Pages from 'vite-plugin-pages'
-import generateSitemap from 'vite-plugin-pages-sitemap'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    // ...
-    Pages({
-      onRoutesGenerated: () => {
-        const explicitRoutes = ['/', '/ueber-uns', '/leistungen', '/produkte', '/kontakt']
-        generateSitemap({ routes: explicitRoutes })
-      }
-    })
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
