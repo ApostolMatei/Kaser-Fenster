3088be54f4243c7137782d5023d2828fec5f73b1
mateias109 <mateias109@gmail.com>
2023-11-13 13:07:31 +0200

    Final

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,7 @@
 <script setup>
-import Slider from '../components/Header-slider.vue'
-
-import Main from '../components/Main-content.vue'
-//
+import Slider from '@/components/Header-slider.vue'
+import Brands from '@/components/Brands-kaser.vue'
+import Main from '@/components/Main-content.vue'
 </script>
 
 <template>
