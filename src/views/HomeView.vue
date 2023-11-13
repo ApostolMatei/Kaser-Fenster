ae585f2014cfae3a801c95624637c70858c6a1bf
mateias109 <mateias109@gmail.com>
2023-11-13 13:04:49 +0200

    Final

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import Slider from '../components/Header-slider.vue'
 import Brands from '../components/Brands-kaser.vue'
 import Main from '../components/Main-content.vue'
+//
 </script>
 
 <template>
