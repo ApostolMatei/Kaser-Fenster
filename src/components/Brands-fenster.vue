5edec25126e6d7148e4cc6402b9cc379a5c1f05a
mateias109 <mateias109@gmail.com>
2023-11-15 10:34:02 +0200

    Final

--- src/components/Brands-fenster.vue
+++ src/components/Brands-fenster.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="background">
     <div class="brands" ref="slider" @click="muian">
-      <div class="inner" ref="inner">
-        <div class="logos" v-for="index in 3" :key="index">
-          <img
-            width="120"
-            height="90"
-            class="brand"
-            v-for="brand in brands"
-            :key="brand"
-            :src="brand.src"
-            :alt="brand.alt"
-          />
+      <div class="mask">
+        <div class="inner" ref="inner">
+          <div class="logos" v-for="index in 3" :key="index">
+            <img
+              width="120"
+              height="90"
+              class="brand"
+              v-for="brand in brands"
+              :key="brand"
+              :src="brand.src"
+              :alt="brand.alt"
+            />
+          </div>
         </div>
+        <div class="gradient-mask"></div>
       </div>
     </div>
   </div>
@@ -40,6 +43,31 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.mask {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.gradient-mask {
+  position: absolute;
+  // width: 100%;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgb(0, 0, 0);
+  background: rgb(0, 0, 0);
+  background: rgb(0, 0, 0);
+  background: linear-gradient(
+    90deg,
+    rgba(0, 0, 0, 0.8883928571428571) 0%,
+    rgba(0, 0, 0, 0) 50%,
+    rgba(0, 0, 0, 0.8841911764705882) 100%
+  );
+  pointer-events: none;
+}
 .logos {
   padding-right: var(--gap);
   display: flex;
@@ -66,7 +94,7 @@ export default {
 
   overflow: hidden;
   margin: 0 auto;
-  max-width: 1600px;
+  max-width: 1900px;
 
   img {
     width: 120px;
