f253cb0584d76acc0b9788661dead7c7475edb0d
mateias109 <mateias109@gmail.com>
2023-11-29 15:22:11 +0200

    Final

--- netlify-functions/sendEmail.js
+++ netlify-functions/sendEmail.js
@@ -1,25 +1,27 @@
-// netlify-functions/sendEmail.js
 const { google } = require('googleapis');
 const MailComposer = require('nodemailer/lib/mail-composer');
 
 exports.handler = async function (event, context) {
   try {
-    const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN,} = process.env;
+    const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN } = process.env;
 
     const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
     oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
-    
+    const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+
+    const formData = JSON.parse(event.body); // Assuming form data is in JSON format
+    const { name, email, message } = formData;
 
     const fileAttachments = [];
 
     const options = {
       to: 'office@kaser-fenster.at',
       cc: '',
-      replyTo: 'mateias109@gmail.com',
-      subject: 'Hello Amit 🚀',
-      text: 'This email is sent from the command line',
-      html: `<p>🙋🏻‍♀️  &mdash; This is a <b>test email</b> from <a href="https://digitalinspiration.com">Digital Inspiration</a>.</p>`,
+      replyTo: email,
+      subject: `New Contact Form Submission from ${name}`,
+      text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}`,
+      html: `<p><b>New Contact Form Submission</b></p><p><b>Name:</b> ${name}</p><p><b>Email:</b> ${email}</p><p><b>Message:</b> ${message}</p>`,
       attachments: fileAttachments,
       textEncoding: 'base64',
       headers: [
@@ -28,11 +30,20 @@ exports.handler = async function (event, context) {
       ],
     };
 
-    // Continue with the rest of your logic...
+    const mailComposer = new MailComposer(options);
+    const messageContent = await mailComposer.compile().build();
+    const rawMessage = Buffer.from(messageContent).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+
+    const { data: { id } } = await gmail.users.messages.send({
+      userId: 'me',
+      resource: {
+        raw: rawMessage,
+      },
+    });
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ success: true, messageId }),
+      body: JSON.stringify({ success: true, messageId: id }),
     };
   } catch (error) {
     console.error('Error sending email:', error);
@@ -40,4 +51,5 @@ exports.handler = async function (event, context) {
       statusCode: 500,
       body: JSON.stringify({ success: false, error: error.message }),
     };
-}}
+  }
+};
