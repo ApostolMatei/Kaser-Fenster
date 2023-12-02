89c53e8b7edb271e49a9069a6cdbfbb0e82ae3af
mateias109 <mateias109@gmail.com>
2023-12-02 16:21:57 +0200

    hau

--- functions/token.js
+++ functions/token.js
@@ -8,15 +8,17 @@ const code = '4/0AfJohXku1pKr6JJVC45UYtdPR9IinrI64P5OniZeChmszXP2cAy1PiYeBwRCDIK
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const redirectUri = process.env.REDIRECT_URI;
-console.log(clientId,clientSecret)
 
-
-
+console.log(clientId, clientSecret);
 
 const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
 
-oAuth2Client.getToken(code).then(({ tokens }) => {
-  const tokenPath = path.join(__dirname, 'token.json');
-  fs.writeFileSync(tokenPath, JSON.stringify(tokens));
-  console.log('Access token and refresh token stored to token.json');
-});
+oAuth2Client.getToken(code)
+  .then(({ tokens }) => {
+    const tokenPath = path.join(__dirname, 'token.json');
+    fs.writeFileSync(tokenPath, JSON.stringify(tokens));
+    console.log('Access token and refresh token stored to token.json');
+  })
+  .catch((error) => {
+    console.error('Error getting token:', error);
+  });
