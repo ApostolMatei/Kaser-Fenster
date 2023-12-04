601adb45a27e3fe74e745b192b2b5ce1965dac4d
mateias109 <mateias109@gmail.com>
2023-12-04 20:51:07 +0200

    suparare

--- functions/token.js
+++ functions/token.js
@@ -1,13 +1,6 @@
-// token.js
 const { google } = require('googleapis');
 const path = require('path');
 const fs = require('fs');
-
-// Replace with the code you received from Google
-const code = '4/0AfJohXku1pKr6JJVC45UYtdPR9IinrI64P5OniZeChmszXP2cAy1PiYeBwRCDIKp2jFEWA';
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
-const redirectUri = process.env.REDIRECT_URI;
 
 exports.handler = async function (event, context) {
   try {
@@ -20,7 +13,17 @@ exports.handler = async function (event, context) {
 
     try {
       const { tokens } = await oAuth2Client.getToken(code);
+
+      // Specify the path to the token.json file
       const tokenPath = path.join(__dirname, 'token.json');
+
+      // Ensure the directory exists
+      const directory = path.dirname(tokenPath);
+      if (!fs.existsSync(directory)) {
+        fs.mkdirSync(directory, { recursive: true });
+      }
+
+      // Write the tokens to the file
       fs.writeFileSync(tokenPath, JSON.stringify(tokens));
       console.log('Access token and refresh token stored to token.json');
 
