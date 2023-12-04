32d6b88051edf9e963756253884c56567efa4592
mateias109 <mateias109@gmail.com>
2023-12-04 16:46:19 +0200

    123

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,43 +1,34 @@
+// functions/sendEmail.js
 const { google } = require('googleapis');
 const MailComposer = require('nodemailer/lib/mail-composer');
+const fs = require('fs');
+const path = require('path');
 
 exports.handler = async function (event, context) {
   try {
     const clientId = process.env.CLIENT_ID;
     const clientSecret = process.env.CLIENT_SECRET;
     const redirectUri = process.env.REDIRECT_URI;
-    
 
     const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
-    oAuth2Client.setCredentials({ refresh_token: accessToken });
 
-    console.log('CLIENT_ID:', process.env.CLIENT_ID);
-    console.log('CLIENT_SECRET:', process.env.CLIENT_SECRET);
-    console.log('REDIRECT_URI:', process.env.REDIRECT_URI);
-    
+    // Load the tokens from token.json
+    const tokenPath = path.join(__dirname, 'token.json');
+    const tokens = JSON.parse(fs.readFileSync(tokenPath, 'utf-8'));
+    oAuth2Client.setCredentials(tokens);
 
     const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
 
-    const formData = JSON.parse(event.body); // Assuming form data is in JSON format
-    const { name, email, message } = formData;
-
-    console.log('Received form data:', formData);
-
-    const fileAttachments = [];
+     
+    const toEmail = 'mateias109@gmail.com';
 
     const options = {
-      to: 'office@kaser-fenster.at',
+      to: toEmail,
       cc: '',
-      replyTo: email,
-      subject: `New Contact Form Submission from ${name}`,
-      text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}`,
-      html: `<p><b>New Contact Form Submission</b></p><p><b>Name:</b> ${name}</p><p><b>Email:</b> ${email}</p><p><b>Message:</b> ${message}</p>`,
-      attachments: fileAttachments,
-      textEncoding: 'base64',
-      headers: [
-        { key: 'X-Application-Developer', value: 'Amit Agarwal' },
-        { key: 'X-Application-Version', value: 'v1.0.0.2' },
-      ],
+      replyTo: toEmail,
+      subject: 'Test Email',
+      text: 'This is a test email.',
+      html: '<p>This is a test email.</p>',
     };
 
     const mailComposer = new MailComposer(options);
@@ -50,6 +41,7 @@ exports.handler = async function (event, context) {
         raw: rawMessage,
       },
     });
+
 
     return {
       statusCode: 200,
--- functions/token.js
+++ functions/token.js
@@ -1,24 +1,30 @@
-// token.js
+// functions/getToken.js
 const { google } = require('googleapis');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 
-// Replace with the code you received from Google
-const code = '4/0AfJohXku1pKr6JJVC45UYtdPR9IinrI64P5OniZeChmszXP2cAy1PiYeBwRCDIKp2jFEWA';
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
-const redirectUri = process.env.REDIRECT_URI;
+exports.handler = async function (event, context) {
+  try {
+    const code = event.queryStringParameters.code;
+    const clientId = process.env.CLIENT_ID;
+    const clientSecret = process.env.CLIENT_SECRET;
+    const redirectUri = process.env.REDIRECT_URI;
 
-console.log(clientId, clientSecret);
+    const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
 
-const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
-
-oAuth2Client.getToken(code)
-  .then(({ tokens }) => {
+    const { tokens } = await oAuth2Client.getToken(code);
     const tokenPath = path.join(__dirname, 'token.json');
     fs.writeFileSync(tokenPath, JSON.stringify(tokens));
-    console.log('Access token and refresh token stored to token.json');
-  })
-  .catch((error) => {
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true, tokens }),
+    };
+  } catch (error) {
     console.error('Error getting token:', error);
-  });
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ success: false, error: error.message }),
+    };
+  }
+};
