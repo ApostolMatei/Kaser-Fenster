d3ef763a5b55ab024b1fb705b9f43f3d3c65e44a
mateias109 <mateias109@gmail.com>
2023-12-04 20:45:53 +0200

    123

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,40 +1,56 @@
-// functions/sendEmail.js
 const { google } = require('googleapis');
 const MailComposer = require('nodemailer/lib/mail-composer');
+const fs = require('fs');
+const path = require('path');
 
 exports.handler = async function (event, context) {
   try {
-    const apiKey = process.env.GMAIL_API_KEY;
-    const gmail = google.gmail({ version: 'v1', auth: apiKey });
+    const clientId = process.env.CLIENT_ID;
+    const clientSecret = process.env.CLIENT_SECRET;
+    const redirectUri = process.env.REDIRECT_URI;
 
-    const toEmail = 'office@kaser-fenster.at'; // Use your desired recipient email address
+    const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
+
+    // Load the tokens from token.json
+    const tokenPath = path.join(__dirname, 'token.json');
+    const tokens = JSON.parse(await fs.promises.readFile(tokenPath, 'utf-8'));
+    oAuth2Client.setCredentials(tokens);
+
+    const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+
+    const formData = JSON.parse(event.body); // Assuming form data is in JSON format
+    const { name, email, message } = formData;
+
+    console.log('Received form data:', formData);
+
+    const fileAttachments = [];
+
+    const toEmail = 'mateias109@gmail.com';
 
     const options = {
       to: toEmail,
       cc: '',
-      replyTo: toEmail,
-      subject: 'Contact Form Submission',
-      text: `Name: ${event.body.name}\nEmail: ${event.body.email}\n\nMessage: ${event.body.message}`,
-      html: `<p><b>Contact Form Submission</b></p><p><b>Name:</b> ${event.body.name}</p><p><b>Email:</b> ${event.body.email}</p><p><b>Message:</b> ${event.body.message}</p>`,
+      replyTo: email,
+      subject: `New Contact Form Submission from ${name}`,
+      text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}`,
+      html: `<p><b>New Contact Form Submission</b></p><p><b>Name:</b> ${name}</p><p><b>Email:</b> ${email}</p><p><b>Message:</b> ${message}</p>`,
+      attachments: fileAttachments,
+      textEncoding: 'base64',
+      headers: [
+        { key: 'X-Application-Developer', value: 'Amit Agarwal' },
+        { key: 'X-Application-Version', value: 'v1.0.0.2' },
+      ],
     };
 
     const mailComposer = new MailComposer(options);
-    const messageContent = await mailComposer.compile().build();
-    const rawMessage = Buffer.from(messageContent).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-
-    const { data: { id } } = await gmail.users.messages.send({
-      userId: 'me',
-      resource: {
-        raw: rawMessage,
-      },
-    });
+    // Rest of your code for sending email...
 
     return {
       statusCode: 200,
       body: JSON.stringify({ success: true, messageId: id }),
     };
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error('Error:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ success: false, error: error.message }),
--- functions/token.js
+++ functions/token.js
@@ -1,7 +1,13 @@
-// functions/getToken.js
+// token.js
 const { google } = require('googleapis');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
+
+// Replace with the code you received from Google
+const code = '4/0AfJohXku1pKr6JJVC45UYtdPR9IinrI64P5OniZeChmszXP2cAy1PiYeBwRCDIKp2jFEWA';
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const redirectUri = process.env.REDIRECT_URI;
 
 exports.handler = async function (event, context) {
   try {
@@ -12,16 +18,25 @@ exports.handler = async function (event, context) {
 
     const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
 
-    const { tokens } = await oAuth2Client.getToken(code);
-    const tokenPath = path.join(__dirname, 'token.json');
-    fs.writeFileSync(tokenPath, JSON.stringify(tokens));
+    try {
+      const { tokens } = await oAuth2Client.getToken(code);
+      const tokenPath = path.join(__dirname, 'token.json');
+      fs.writeFileSync(tokenPath, JSON.stringify(tokens));
+      console.log('Access token and refresh token stored to token.json');
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ success: true, tokens }),
-    };
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ success: true, tokens }),
+      };
+    } catch (error) {
+      console.error('Error getting token:', error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ success: false, error: error.message }),
+      };
+    }
   } catch (error) {
-    console.error('Error getting token:', error);
+    console.error('Error:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ success: false, error: error.message }),
